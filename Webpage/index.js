58a2621dd51343041de96f375e188abfb39e7fe0
Kraton <maduvan.kasi@mail.utoronto.ca>
2018-12-02 15:28:26 -0500

    Chatting feature - direct and anonymous

--- Webpage/index.js
+++ Webpage/index.js
@@ -7,6 +7,8 @@ const port = 8080
 const crypto = require('crypto')
 const salt = "REDACTED"
 
+const chance = require('chance').Chance()
+
 const fs = require('fs')
 const groupsFileName = "./groups.json"
 const tokensFileName = "./activeTokens.json"
@@ -22,7 +24,7 @@ function refreshFile(fileName) {
 	return require(fileName)
 }
 
-function findName(token) {
+function findNameByToken(token) {
 	var tokensFile = refreshFile(tokensFileName)
 	for (var user in tokensFile) {
 		if (tokensFile[user] == token) {
@@ -30,6 +32,26 @@ function findName(token) {
 		}
 	}
 	return undefined
+}
+
+function findNameByAlias(alias, chatList) {
+	for (var chatter in chatList) {
+		if (chatList[chatter].sender == alias) {
+			return chatter
+		}
+	}
+	return undefined
+}
+
+function generateAlias(group) {
+	var alias = chance.animal().toLowerCase()
+	alias = alias.charAt(0).toUpperCase() + alias.slice(1)
+	while (group.aliases.includes(alias)){
+		alias = chance.animal().toLowerCase()
+		alias = alias.charAt(0).toUpperCase() + alias.slice(1)
+	}
+	group.aliases.push(alias)
+	return alias
 }
 
 function drawNames(groupsObject, groupName) {
@@ -78,8 +100,9 @@ app.post('/creategroup', (req, res) => {
 	groupBody.members = {}
 	groupBody.drawnNames = false
 	groupBody.eventDate = req.body.date
+	groupBody.aliases = []
 	groupsFile[req.body.groupName] = groupBody
-	fs.writeFile(groupsFileName, JSON.stringify(groupsFile), defaultCallback)
+	fs.writeFile(groupsFileName, JSON.stringify(groupsFile, null, 2), defaultCallback)
 	res.sendStatus(200)
 })
 
@@ -96,7 +119,7 @@ app.post('/signin', (req, res) => {
 		if (groupsFile[req.body.group].members[req.body.user].key == hash.digest('hex')){
 			var token = crypto.randomBytes(16).toString('hex')
 			tokensFile[req.body.user] = token
-			fs.writeFile(tokensFileName, JSON.stringify(tokensFile), defaultCallback)
+			fs.writeFile(tokensFileName, JSON.stringify(tokensFile, null, 2), defaultCallback)
 			res.send(token)
 		} else {
 			res.sendStatus(403)
@@ -113,20 +136,22 @@ app.post('/signup', (req, res) => {
 
 	var memberBody = {}
 	memberBody.key = hash.digest('hex')
-	memberBody.questions = {}
+	memberBody.sentChats = {}
+	memberBody.receivedChats = {}
 	memberBody.wishlist = {}
 	groupsFile[req.body.group].members[req.body.user] = memberBody
-	fs.writeFile(groupsFileName, JSON.stringify(groupsFile), defaultCallback)
+	fs.writeFile(groupsFileName, JSON.stringify(groupsFile, null, 2), defaultCallback)
 
 	var token = crypto.randomBytes(16).toString('hex')
 	tokensFile[req.body.user] = token
-	fs.writeFile(tokensFileName, JSON.stringify(tokensFile), defaultCallback)
+	fs.writeFile(tokensFileName, JSON.stringify(tokensFile, null, 2), defaultCallback)
 	res.send(token)
 })
 
 app.post('/home', (req, res) => {
 	var groupsFile = refreshFile(groupsFileName)
 	var groupName = req.body.group
+	
 	var responseMsg = "Here is your group:\n"
 	for (var member in groupsFile[groupName].members) {
 		responseMsg += member + "\n"
@@ -135,7 +160,7 @@ app.post('/home', (req, res) => {
 		responseMsg += "Your group hasn't drawn names yet."
 		res.status(201)
 	} else {
-		var member = findName(req.body.token)
+		var member = findNameByToken(req.body.token)
 		responseMsg += `You are giving a gift to ${groupsFile[groupName].members[member].giftGivee}!`
 	}
 	res.send(responseMsg)
@@ -147,9 +172,84 @@ app.post('/draw', (req, res) => {
 
 	drawNames(groupsFile, groupName)
 	groupsFile[groupName].drawnNames = true
-	fs.writeFile(groupsFileName, JSON.stringify(groupsFile), defaultCallback)
+	fs.writeFile(groupsFileName, JSON.stringify(groupsFile, null, 2), defaultCallback)
 
 	res.sendStatus(200)
+})
+
+app.post('/chats', (req, res) => {
+	var groupsFile = refreshFile(groupsFileName)
+	var user = groupsFile[req.body.group].members[findNameByToken(req.body.token)]
+
+	var response = "Sent Chats:\n"
+	for (var chat in user.sentChats) {
+		response += chat + "\n"
+	}
+	response += "Received Chats:\n"
+	for (var chat in user.receivedChats) {
+		response += user.receivedChats[chat].sender + "\n"
+	}
+
+	res.send(response)
+})
+
+app.post('/viewChat', (req, res) => {
+	var groupsFile = refreshFile(groupsFileName)
+	var user = groupsFile[req.body.group].members[findNameByToken(req.body.token)]
+
+	var chat
+	if (req.body.type == "sent") {
+		chat = user.sentChats[req.body.chatee]
+	} else if (req.body.type == "received") {
+		chat = user.receivedChats[findNameByAlias(req.body.chatee, user.receivedChats)]
+	}
+
+	var response = "Chats:\n"
+	while (chat.next != undefined) {
+		response += (chat.sender + ": " + chat.message + "\n")
+		chat = chat.next
+	}
+	response += (chat.sender + ": " + chat.message)
+
+	res.send(response)
+})
+
+app.post('/sendChat', (req, res) => {
+	var groupsFile = refreshFile(groupsFileName)
+	var user = groupsFile[req.body.group].members[findNameByToken(req.body.token)]
+	
+	if (req.body.create == false) {
+		var chat, otherChat, userAlias
+		if (req.body.type == "sent") {
+			var partnerName = req.body.chatee
+			chat = user.sentChats[partnerName]
+			otherChat = groupsFile[req.body.group].members[partnerName].receivedChats[findNameByToken(req.body.token)]
+			userAlias = otherChat.sender
+		} else if (req.body.type == "received") {
+			var partnerName = findNameByAlias(req.body.chatee, user.receivedChats)
+			chat = user.receivedChats[partnerName]
+			otherChat = groupsFile[req.body.group].members[partnerName].sentChats[findNameByToken(req.body.token)]
+			userAlias = findNameByToken(req.body.token)
+		}
+		
+		while (chat.next != undefined) {
+			chat = chat.next
+		}
+		chat.next = {"sender": findNameByToken(req.body.token), "message": req.body.message}
+
+		while (otherChat.next != undefined) {
+			otherChat = otherChat.next
+		}
+		otherChat.next = {"sender": userAlias, "message": req.body.message}
+	} else {
+		var partnerName = req.body.chatee
+		user.sentChats[partnerName] = {"sender": findNameByToken(req.body.token), "message": req.body.message}
+		groupsFile[req.body.group].members[partnerName].receivedChats[findNameByToken(req.body.token)] = {"sender": generateAlias(groupsFile[req.body.group]), "message": req.body.message}
+	}
+
+	fs.writeFile(groupsFileName, JSON.stringify(groupsFile, null, 2), defaultCallback)
+	res.sendStatus(200)
+	
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
